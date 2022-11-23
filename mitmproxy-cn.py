2f88ac4041f34dde87d3b1fa04ae3c8faeec982f
Loli-Killer <38392948+Loli-Killer@users.noreply.github.com>
2022-11-23 02:26:15 -0500

    Fix mitmproxy

--- mitmproxy-cn.py
+++ mitmproxy-cn.py
@@ -10,16 +10,18 @@ class AKRedirect:
 
     def http_connect(self, flow: mitmproxy.http.HTTPFlow):
         if 'ak-conf.hypergryph.com' in flow.request.pretty_host:
-            flow.request.host = "localhost"
+            flow.request.scheme = 'http'
+            flow.request.host = "127.0.0.1"
+            flow.request.port = 8443
 
     def request(self, flow: mitmproxy.http.HTTPFlow):
         if 'bi-track.hypergryph.com' in flow.request.pretty_host:
             flow.request.scheme = 'http'
-            flow.request.host = "localhost"
+            flow.request.host = "127.0.0.1"
 
         if 'ak-conf.hypergryph.com' in flow.request.pretty_host:
             flow.request.scheme = 'http'
-            flow.request.host = "localhost"
+            flow.request.host = "127.0.0.1"
             flow.request.port = 8443
 
 addons = [
