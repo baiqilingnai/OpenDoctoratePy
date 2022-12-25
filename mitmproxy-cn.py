36709352bd49ecb37c136ba91df5e777fa55802b
Loli-Killer <38392948+Loli-Killer@users.noreply.github.com>
2022-12-25 16:26:48 -0500

    disable tls

--- mitmproxy-cn.py
+++ mitmproxy-cn.py
@@ -1,5 +1,6 @@
 import json
 import mitmproxy.http
+import mitmproxy.proxy.context
 
 server = json.load(open('./config/config.json', 'r'))["server"]
 
@@ -21,7 +22,17 @@ class AKRedirect:
     def __init__(self):
         print('Addon for Redirecting Arknight [EN] Loaded !')
 
+    def server_connect(self, data: mitmproxy.proxy.context.Context):
+        data.server.tls = False
+
     def http_connect(self, flow: mitmproxy.http.HTTPFlow):
+        for domain in self.DOMAINS_LIST:
+            if domain in flow.request.pretty_host:
+                flow.request.scheme = 'http'
+                flow.request.host = host
+                flow.request.port = port
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
         for domain in self.DOMAINS_LIST:
             if domain in flow.request.pretty_host:
                 if domain == 'ak-conf.hypergryph.com':
@@ -29,9 +40,7 @@ class AKRedirect:
                     flow.request.host = host
                     flow.request.port = port
                 else:
-                    flow.request.host = '0.0.0.0'
-
-    request = http_connect
+                    flow.response = mitmproxy.http.Response.make(200, b"")
 
 addons = [
     AKRedirect()
--- start_mitmproxy.py
+++ start_mitmproxy.py
@@ -6,5 +6,5 @@ mitmproxy_server = json.load(open('./config/config.json', 'r'))["mitmproxy"]
 mitmproxy_port = mitmproxy_server["port"]
 
 os.system(
-    f"mitmdump.exe --listen-host 127.0.0.1 --listen-port {mitmproxy_port} -s mitmproxy-cn.py"
+    f"mitmdump.exe --set connection_strategy=lazy --listen-host 127.0.0.1 --listen-port {mitmproxy_port} -s mitmproxy-cn.py"
 )
